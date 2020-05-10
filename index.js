a9a3ed0b4665ad62b1e47b9a6d879ddf9952e3ac
artigas <artigas@pm.me>
2020-05-10 16:06:28 -0300

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = 
+`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta lang="en-us">
+    <title>Space Shooter</title>
+    <script src="js/phaser.js"></script> <!-- the file name should be the same as the Phaser script you added. -->
+  </head>
+
+  <body>
+    <script src="js/SceneMainMenu.js"></script>
+    <script src="js/SceneMain.js"></script>
+    <script src="js/SceneGameOver.js"></script>
+    <script src="js/game.js"></script>
+  </body>
+</html>
+`;
